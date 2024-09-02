5a527d7d5c4b6ee38949f264e8e0bb3704e29efd
jameslazo <james.lazo@gmail.com>
2024-09-02 10:39:53 -0700

    refactor crmControllers with async/await and validate MongoDB POST

--- src/controllers/crmControllers.js
+++ src/controllers/crmControllers.js
@@ -3,13 +3,15 @@ import { ContactSchema } from "../models/crmModel";
 
 const Contact = mongoose.model("Contact", ContactSchema);
 
-export const addNewContact = (req, res) => {
-    let addNewContact = new Contact(req.body);
+export const addNewContact = async (req, res) => {
+    try {
+        let addNewContact = new Contact(req.body);
 
-    addNewContact.save((err, contact) => {
-        if (err) {
-            res.send(err);
-        }
+        let contact = await addNewContact.save();
+            
         res.json(contact);
-    });
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 };
